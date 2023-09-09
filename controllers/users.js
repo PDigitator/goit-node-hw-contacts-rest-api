948ee2d01c0371d33f5675fd90ae33caf230eca5
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-09-10 01:41:00 +0300

    Add controller updateAvatar and resized with Jimp

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
@@ -85,11 +86,21 @@ const updateSubscriptionUser = async (req, res) => {
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
+
+  const tempAvatar = await Jimp.read(tempUpload);
+  tempAvatar.resize(250, 250).write(tempUpload);
+
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
+  await User.findByIdAndUpdate(
+    _id,
+    { avatarURL },
+    {
+      new: true,
+    }
+  );
 
   res.json({ avatarURL });
 };
